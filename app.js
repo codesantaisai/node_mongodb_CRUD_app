bcd638797118967fc5637612bd3d57f2689a49f2
Jathurshan Santhirasekaram <124557050+codesantaisai@users.noreply.github.com>
2023-07-19 17:08:21 +0530

    delete functionupdated

--- app.js
+++ app.js
@@ -2,43 +2,73 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const exhbs = require('express-handlebars');
-const dbo = require('./db')
+const dbo = require('./db');
+// const ObjectId = dbo.ObjectId;
+const {
+    ObjectId
+} = require('mongodb');
 
 
 app.engine('hbs', exhbs.engine({
     layoutsDir: 'views/',
     defaultLayout: 'main',
     extname: 'hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 
 app.get('/', async (req, res) => {
-
     let database = await dbo.getDatabase();
-    const collection = database.collection('books')
+    const collection = database.collection('books');
     const cursor = collection.find({});
     let books = await cursor.toArray();
 
     let message = '';
+    let edit_id = '';
+    let edit_book = '';
+
+    if (req.query.edit_id) {
+        edit_id = req.query.edit_id;
+        edit_book = await collection.findOne({
+            _id: new ObjectId(edit_id)
+        });
+    }
+    if (req.query.delete_id) {
+        delete_id = req.query.delete_id;
+        edit_book = await collection.deleteOne({
+            _id: new ObjectId(delete_id)
+        });
+        return res.redirect('/?status=3');
+
+    }
+
 
     switch (req.query.status) {
         case '1':
-            message = "inserted Successfully";
+            message = "Inserted Successfully!";
+            break;
+        case '2':
+            message = 'Updated Successfully!';
+            break;
+        case '3':
+            message = 'Deleted Successfully!';
             break;
 
         default:
             break;
     }
+
     res.render('main', {
         message,
-        books
-    })
-
-})
+        books,
+        edit_id,
+        edit_book
+    });
+});
 
 app.post('/store_book', async (req, res) => {
     let database = await dbo.getDatabase();
@@ -46,12 +76,29 @@ app.post('/store_book', async (req, res) => {
     let book = {
         title: req.body.title,
         author: req.body.author
-    }
+    };
     await collection.insertOne(book);
-    return res.redirect('/?status=1');
-})
 
+    return res.redirect('/?status=1');
+});
+
+app.post('/update_book/:edit_id', async (req, res) => {
+    let database = await dbo.getDatabase();
+    const collection = database.collection('books');
+    let book = {
+        title: req.body.title,
+        author: req.body.author
+    };
+    let edit_id = req.params.edit_id;
+
+    await collection.updateOne({
+        _id: ObjectId(edit_id)
+    }, {
+        $set: book
+    });
+    return res.redirect('/?status=2');
+});
 
 app.listen(3000, () => {
-    console.log("server is running on port:3000");
-})
+    console.log("Running port: 3000");
+});
--- db.js
+++ db.js
@@ -1,19 +1,21 @@
 const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient; //using mongoclient class from mongodb
+const MongoClient = mongodb.MongoClient;
+const ObjectID = mongodb.ObjectId;
 
 let database;
 
 async function getDatabase() {
-    const client = await MongoClient.connect('mongodb://127.0.0.1:27017')
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
     database = client.db('library');
 
     if (!database) {
-        console.log("Database not connected");
+        console.log('Database not connected');
     }
-    
+
     return database;
 }
 
 module.exports = {
-    getDatabase
+    getDatabase,
+    ObjectID
 }
